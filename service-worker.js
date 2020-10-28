3568be1c6a6feee916244c85260e6f7cc4f2e881
nicogulo <nicogulobelas@gmail.com>
2020-10-28 23:13:45 +0700

    added workbox

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
-
 if (workbox) {
   console.log(`Workbox berhasil dimuat`);
   workbox.precaching.precacheAndRoute([
